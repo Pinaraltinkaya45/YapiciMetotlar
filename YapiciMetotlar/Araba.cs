54a8c81b5d97888ac44d5bd52651c991c2b06452
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-11-30 16:59:53 +0300

    Add project files.

---
+++ YapiciMetotlar/Araba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapiciMetotlar
+{
+    public class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+
+        public Araba(int _kapiSayisi, string _arabaModel,string arabaRengi)
+        {
+            this.kapiSayisi = _kapiSayisi;
+            this.arabaModel = _arabaModel;
+            this.arabaRengi = arabaRengi;
+        }
+
+        public void motorCalistir()
+        {
+            Console.WriteLine("Motor Çalışıyor..");
+        }
+
+        public void kapilariKitle()
+        {
+            Console.WriteLine("Kapilar Kitleniyor..");
+        }
+    }
+}
---
+++ YapiciMetotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapiciMetotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba a1 = new Araba(4, "BMW", "Kırmızı");
+
+            Console.WriteLine("Arabanın Kapi Sayisi: " + a1.kapiSayisi);
+            Console.WriteLine("Arabanın modeli: " + a1.arabaModel);
+            Console.WriteLine("Arabanın rengi : " + a1.arabaRengi);
+
+            a1.motorCalistir();
+            a1.kapilariKitle();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
